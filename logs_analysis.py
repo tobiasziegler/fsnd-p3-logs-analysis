c4377b5c3d77f0d3c82a42933283ef86a138b4b7
David Mallard <correspondence@davidmallard.id.au>
2017-10-22 16:59:12 +1100

    Create a function to execute the database queries

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
 import psycopg2
+
+def execute_query(query):
+    """Connect to the database, execute the specified query and return the
+    results."""
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
