d488af07fad5d3d1fe460840140f87093b8ad599
David Mallard <correspondence@davidmallard.id.au>
2017-10-22 18:17:13 +1100

    Create the main program that will run queries and output results

--- logs_analysis.py
+++ logs_analysis.py
@@ -11,3 +11,9 @@ def execute_query(query):
     results = c.fetchall()
     db.close()
     return results
+
+# The main program that performs the analyses when this file is run
+if __name__ == '__main__':
+    print("1. What are the most popular three articles of all time?")
+    print("2. Who are the most popular article authors of all time?")
+    print("3. On which days did more than 1%% of requests lead to errors?")
