969e9facf583ffde0738448870d90c5c1b60bb71
David Mallard <correspondence@davidmallard.id.au>
2017-10-22 18:37:37 +1100

    Create the query to answer the second question

--- logs_analysis.py
+++ logs_analysis.py
@@ -22,9 +22,20 @@ def get_top_3_articles():
                 limit 3;"""
     return execute_query(query1)
 
+def get_top_authors():
+    """Find the most popular article authors."""
+    query2 = """select name, count(*) as views
+                from authors, articles, log
+                where authors.id = articles.author
+                and log.path like '%' || articles.slug
+                group by name
+                order by views desc;"""
+    return execute_query(query2)
+
 # The main program that performs the analyses when this file is run
 if __name__ == '__main__':
     print("1. What are the most popular three articles of all time?")
     print(get_top_3_articles())
     print("2. Who are the most popular article authors of all time?")
+    print(get_top_authors())
     print("3. On which days did more than 1%% of requests lead to errors?")
